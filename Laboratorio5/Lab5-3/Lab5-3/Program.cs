5b70af7ed76454b563bab009467fd96e9319c1f4
TiaraRodriguez <tiara.rodriguez@utp.ac.pa>
2025-09-24 21:48:28 -0500

    lab 5 completo

---
+++ Laboratorio5/Lab5-3/Lab5-3/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//Tiara Rodriguez
+
+class Program
+{
+    static void Main(string[] args)  // main principal
+    {
+        string[] frutas = { "manzana", "platano", "naranja" };
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+        }
+    }
+}
