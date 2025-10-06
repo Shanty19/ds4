e763424164d1e3b3eb2ac74a16292a15338e00a5
TiaraRodriguez <tiara.rodriguez@utp.ac.pa>
2025-10-05 20:01:55 -0500

    Laboratorios Completos

---
+++ Laboratorio2/Laboratorio21/Laboratorio21/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Laboratorio2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client cliente = new Client();
+            cliente.FirstName = "Su_Nombre";
+            cliente.FirstName = "Su_Apellido";
+            cliente.Age = 15;
+            cliente.Id = 1;
+            Console.WriteLine(cliente.GetFullname());
+        }
+    }
+    public class Client
+    {
+        //Declaramos las variables de instancia en clase.
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ushort Age { get; set; }
+
+        public string GetFullname()
+        {
+            //Utilizando variables de instancia dentro de metodos de la clase 
+            return FirstName + " " + LastName;
+        }
+    }
+}
---
+++ Laboratorio8/Lab8-1/Lab8-1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+class Persona
+
+{     
+    // campo de cada objeto Persona que almacena  su nombre
+    public string Nombre;
+    //Campo de cada objeto Persona que almacena su edad
+    public int Edad;
+
+    //Campo de cada objeto Persona que almacena su NIF
+    public string NIF;
+
+    void Cumpleaños()   //Incrementa en uno la edad del objeto Persona
+    {
+        Edad++;
+    }
+    //constructor de persona
+    public Persona(string nombre, int edad,string nif)
+    {
+        Nombre = nombre;
+        Edad = edad;
+        NIF = nif;
+    }
+    
+     
+}
+
+
+class Trabajador : Persona
+{
+    //campo de cada objeto Trabajador que almacena cuánto gana
+    public int Sueldo;
+    
+    public Trabajador(string nombre, int edad, string nif, int sueldo) : base(nombre, edad,nif)
+    {
+        //Inicializamos cada trabajador en base al constructor de Persona
+        Sueldo = sueldo;
+    }
+    
+}
+
+class Program
+{
+    public static void Main()
+    {
+    Trabajador p = new Trabajador("Josan", 22, "77588260-Z", 100000);
+    Console.WriteLine("Nombre=" + p.Nombre);
+    Console.WriteLine("Edad=" + p.Edad);
+    Console.WriteLine("NIF=" + p.NIF);
+    Console.WriteLine("Sueldo=" + p.Sueldo);
+    Console.ReadKey();
+    }
+
+}
